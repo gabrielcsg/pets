5338b0dc1a0011dc43169bbe1abf4623ea985b00
Gabriel <gabrielcssg@gmail.com>
2020-12-18 14:10:33 -0300

    css do form ajustado

--- resources/views/formPet.blade.php
+++ resources/views/formPet.blade.php
@@ -7,19 +7,46 @@
     <title>New</title>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <style type="text/css">
+        form {
+            display: flex;
+            flex-direction: column;
+        }
+
+        input+input {
+            margin-top: 8px;
+        }
+
+        button {
+            margin-top: 20px;
+            cursor: pointer;
+        }
+
+        a {
+            margin-top: 10px;
+            text-align: center;
+        }
+
+        .containerForm {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+        }
+    </style>
 </head>
 
 <body>
     <h1 align="center" style="margin-top: 50px;">New Pet</h1>
-    <div class="container" style="display: flex; align-items: center; justify-content:center; flex-direction: column;">
-        <form method="post" action="/new" style="display:flex; flex-direction: column;">
+    <div class="container containerForm">
+        <form method="post" action="/new">
             @csrf
-            Name: <input type="text" name="name" required style="margin-bottom: 8px;" />
-            Breed: <input type="text" name="breed" required style="margin-bottom: 8px;" />
-            Size: <input type="text" name="size" required style="margin-bottom: 8px;" />
+            Name: <input type="text" name="name" required />
+            Breed: <input type="text" name="breed" required />
+            Size: <input type="text" name="size" required />
             Gender: <input type="text" name="gender" required />
-            <button type="submit" style="margin-top: 20px; cursor: pointer;">Save</button>
-            <a href="/" style="margin-top: 10px; text-align: center;">Cancel</a>
+            <button type="submit">Save</button>
+            <a href="/">Cancel</a>
         </form>
     </div>
 </body>
